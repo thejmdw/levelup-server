214b4bde346fe295814e1547f481be940c3f388d
Jonathan Watson <thejmdw@gmail.com>
2021-08-03 13:45:18 -0400

    through Chapter5

---
+++ levelupapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .gamer import Gamer
+from .game import Game
+from .event_gamer import EventGamer
+from .event import Event
+from .game_type import GameType
---
+++ levelupapi/models/event.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Event(models.Model):
+    host = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    description = models.TextField()
+    title = models.CharField(max_length=50)
